a1e01de29020109c09b7d5338017684daeefd050
Jacob Karla <jacob.l.karla@gmail.com>
2022-09-11 19:01:36 -0400

    added prompts and initial function serving as main menu

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require("inquirer")
 const conTable = require('console.table');
-const sequelize = require('./config/connection');
+
 const PORT = process.env.port || 3001;
 const app = express();
 
@@ -20,3 +20,54 @@ const db = mysql.createConnection(
     console.log(`Connected to the employeeDB database.`)
   );
 
+const init = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            message: "Please select an option",
+            choices: [
+              "View All Employees",
+              "Add Employee",
+              "Update Employee Role",
+              "Update Employee's Manager",
+              "View All Roles",
+              "Add Role",
+              "View All Departments",
+              "Add Department",
+              "Quit"
+            ],
+            name: "mainMenu",
+        }
+
+    ]).then((userOption)=>{
+        switch(userOption.mainMenu){
+            case "View All Employees":
+              viewEmployees()
+              break;
+            case "Add Employee":
+              addEmployee()
+              break;
+            case "Update Employee Role":
+              updateEmployeeRole()
+              break;
+            case "Update Employee's Manager":
+              updateEmployeeMan()
+              break;
+            case "View All Roles":
+              viewAllRoles()
+              break;
+            case "Add Role":
+              addRole()
+              break;
+            case "View All Departments":
+              viewAllDepts()
+              break;
+            case "Add Department":
+              addDept()
+              break;
+            case "Quit":
+              quit();
+          }
+        })
+      }
+      init();
