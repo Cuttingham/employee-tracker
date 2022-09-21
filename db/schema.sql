115c7d686129efa3fc6244330f86b35e43a08ca1
Jacob Karla <jacob.l.karla@gmail.com>
2022-09-21 16:53:27 -0400

    added foreign key relationship for tables in sql

--- db/schema.sql
+++ db/schema.sql
@@ -12,13 +12,13 @@ CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NULL,
   salary DECIMAL(10,2) NULL,
-  department_id INT NOT NULL
+  FOREIGN KEY (department_id) REFERENCES department(id) INT NOT NULL
 );
 
 CREATE TABLE employees (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) INT,
     manager_id INT
 )
